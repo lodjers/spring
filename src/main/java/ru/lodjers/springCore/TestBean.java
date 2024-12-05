01aea14a29bb42e9b814a4de7b9cc70f7e59e925
lodjers <officialkovtun@gmail.com>
2024-12-05 21:46:41 +0300

    добавили toString в TestBean.java

--- src/main/java/ru/lodjers/springCore/TestBean.java
+++ src/main/java/ru/lodjers/springCore/TestBean.java
@@ -14,4 +14,11 @@ public class TestBean {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "TestBean{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
