9e228fcd76678496e9688619517bd916773f218a
lodjers <officialkovtun@gmail.com>
2024-12-07 16:43:50 +0300

    добавили scope prototype вместо singleton(при вызове метода getBean теперь создаётся каждый раз новый объект, singleton переменные ссылались на один и тот же объект)

--- src/main/java/ru/lodjers/springCore/TestSpring.java
+++ src/main/java/ru/lodjers/springCore/TestSpring.java
@@ -9,9 +9,11 @@ public class TestSpring {
 //        Music music = context.getBean("musicBean", Music.class);
 
 //        MusicPlayer musicPlayer  = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer1.setVolume(10);
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
 
         context.close();
     }
