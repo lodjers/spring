0b4cab27237b8e70c9d92c6b3ba66920b2197e7a
lodjers <officialkovtun@gmail.com>
2024-12-07 18:01:14 +0300

    добавили init(при инициализации bean), destroy(при уничтожении), и factory(при создании объектов bean) методы

--- src/main/java/ru/lodjers/springCore/TestSpring.java
+++ src/main/java/ru/lodjers/springCore/TestSpring.java
@@ -6,14 +6,16 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
+        ClassicalMusic classicalMusic = context.getBean("ClassicalMusicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 //        Music music = context.getBean("musicBean", Music.class);
 
 //        MusicPlayer musicPlayer  = new MusicPlayer(music);
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer1.setVolume(10);
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer1.setVolume(10);
+//        System.out.println(musicPlayer1.getVolume());
+//        System.out.println(musicPlayer2.getVolume());
 
         context.close();
     }
