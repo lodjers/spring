27527757467d57b65a46c9e4b53b8bc6742e33ab
lodjers <officialkovtun@gmail.com>
2024-12-07 19:43:21 +0300

    добавили аннотацию Qualifier(уточняет, бин с каким id нужно передать в Autowired. При приминении Qualifier в конструкторе синтаксис меняется!

--- src/main/java/ru/lodjers/springCore/MusicPlayer.java
+++ src/main/java/ru/lodjers/springCore/MusicPlayer.java
@@ -1,44 +1,23 @@
 package ru.lodjers.springCore;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private List<Music> musicList = new ArrayList<>();
-    private String name;
-    private int volume;
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
+    private Music music1;
+    private Music music2;
     @Autowired
-    public void setClassicalMusic(ClassicalMusic classicalMusic) {
-        this.classicalMusic = classicalMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,@Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
-    public void playMusicList() {
-//
-        musicList.forEach(el -> System.out.println("Playing: " + el.getSong()));
-    }
+
 }
--- src/main/java/ru/lodjers/springCore/RockMusic.java
+++ src/main/java/ru/lodjers/springCore/RockMusic.java
@@ -1,7 +1,7 @@
 package ru.lodjers.springCore;
 
 import org.springframework.stereotype.Component;
-
+@Component
 public class RockMusic implements Music{
     @Override
     public String getSong() {
