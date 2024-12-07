ca58877bef99125c4cc138db598459256c851e24
lodjers <officialkovtun@gmail.com>
2024-12-07 12:13:54 +0300

    удалил лишние методы из класса TestBean, начало работы с курсом

--- src/main/java/ru/lodjers/springCore/TestBean.java
+++ src/main/java/ru/lodjers/springCore/TestBean.java
@@ -17,23 +17,5 @@ public class TestBean {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "TestBean{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        TestBean testBean = (TestBean) o;
-        return Objects.equals(name, testBean.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
